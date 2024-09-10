013759d5f5cc83e10f450e73da0709c4cf8369fc
Roman-Octavian <work.octavian@gmail.com>
2024-09-10 15:34:29 +0200

    minor correction

--- cli/day-3/higher-lower.ts
+++ cli/day-3/higher-lower.ts
@@ -2,6 +2,7 @@ import readline from 'readline';
 
 console.log('\n+++++++++++++++++ DAY 3: higher-lower +++++++++++++++++\n');
 
+// Randomly select a number between 1 and 100
 const random_number = Math.floor(Math.random() * 100 + 1);
 
 let lastGuess: number | undefined;
@@ -21,11 +22,14 @@ const takeUserInput = async function (query: string): Promise<string> {
 };
 
 while (true) {
+  // Ask the user for a guess (between 1 and 100)
   const guess = Number(await takeUserInput('Provide a number between 1 and 100\n\n'));
 
   if (guess == null || isNaN(guess) || guess < 1 || guess > 100) {
     console.log(`\n ${guess} is not a valid number!`);
   } else {
+    // Let the user know that they guessed correctly and how many guesses they used, if they guessed
+    // correctly.
     if (guess === random_number) {
       console.log(
         `\nYou got it right! The number was ${random_number}\n\nGuesses used: ${guessCount}`,
@@ -36,6 +40,8 @@ while (true) {
     guessCount += 1;
     lastGuess = guess;
 
+    // Let the user know if the guess is higher or lower than the randomly selected number, and ask
+    // for a new guess, if the guess was not correct.
     console.log(`\n${guess > random_number ? 'LOWER' : 'HIGHER'} than ${guess}!\n`);
   }
 }
